d6fbe2eeca4879f53b597ec9c64c662111367ca4
[КП-52] Турчик Зоряна <zorianaturchik@gmail.com>
2026-04-21 19:36:09 +0300

    add MessageHandler.

---
+++ QuizMaster/Handlers/MessageHandler.cs
@@ -0,0 +1,20 @@
+using System.Threading; 
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace StudyBot.Handlers
+{
+    public class MessageHandler
+    {
+        public static async Task HandleAsync(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            if (message.Text == null) return;
+
+            if (message.Text == "/start")
+            {
+                await MenuHandler.SendMainMenuAsync(botClient, message.Chat.Id, cancellationToken);
+            }
+        }
+    }
+}
